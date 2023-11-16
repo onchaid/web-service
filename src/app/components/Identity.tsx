fb2de88c5466f678bfcd833548f91a1382e9d29d
bizk <santiago.yanzon1999@gmail.com>
2023-11-16 00:38:29 -0300

    added flow connection

--- src/app/components/Identity.tsx
+++ src/app/components/Identity.tsx
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import { Button, Typography } from '@mui/material';
-import { Face, Fingerprint, AccountBalanceWallet, Check } from '@mui/icons-material';
-import { useState } from 'react';
+import { Face, Fingerprint, AccountBalanceWallet, Check, HourglassEmpty, CheckCircle } from '@mui/icons-material';
+import { useState, useEffect } from 'react';
 import Webcam from "react-webcam";
+import css from '../css/home.module.css';
 
 interface IdentificationBoxProps {
   onClose: () => void;
@@ -33,19 +34,78 @@ const IdentificationBox: React.FC<IdentificationBoxProps> = ({ onClose }) => {
     }
   };
 
+  const [loading, setLoading] = useState(false);
+  const [verificationComplete, setVerificationComplete] = useState(false);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  const handleLogin2 = () => {
+    // Your existing login logic
+    // ...
+
+    // Start the verification process
+    setLoading(true);
+    setSubmitDisabled(true);
+
+    // Simulate progress for 5 seconds
+    let progressValue = 0;
+    const interval = setInterval(() => {
+      progressValue += 1;
+      setProgress(progressValue);
+
+      if (progressValue >= 100) {
+        clearInterval(interval);
+        setLoading(false);
+        setVerificationComplete(true);
+        setSubmitDisabled(false);
+      }
+    }, 10);
+  };
+
+  const handleSubmit = () => {
+
+  }
+
+  const [showTick, setShowTick] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+      setShowTick(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div style={{ position: 'fixed', zIndex: 10, top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '20px', background: '#ffffff', borderRadius: '8px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)' }}>
-      <Typography variant="h6" style={{ textAlign: 'center' }}>Identification Box</Typography>
-      <div style={{ textAlign: 'center', margin: '20px' }}>
-        <Face style={{ fontSize: '64px', color: '#333333' }} />
+    <div className={css.customBackground}>
+      <Typography variant="h6" style={{ textAlign: 'center' }}>Verify your identity</Typography>
+      <div style={{ textAlign: 'center', margin: '20px', position: 'relative' }}>
+          <div style={{  top: '50%', left: '50%'}}>
+            <Webcam style={{ width: '100%', height: 'auto', borderRadius: '2.5%' }} />
+            {verificationComplete && (
+              <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: ' rgba(0, 0, 0, 0.5)', borderRadius: '50%', padding: '2px' }}>
+                <CheckCircle style={{ fontSize: '64px', color: 'rgba(255, 255, 255, 1)' }} />
+              </div>
+            )}
+            {//loading && !verificationComplete && (
+              <div style={{ width: '100%', position: 'absolute' }}>
+                <div style={{ backgroundColor: 'white', width: `${progress}%`, height: '20px', borderRadius: '8px' }} />
+              </div>
+            //)
+            }
+          </div>
       </div>
-      <Webcam allowTransparency />;
-      {walletConnected ? <Typography className="relative flex" variant="h5" style={{color: "black"}}> wellcome {account}</ Typography> : <></>}
-      <Button variant="outlined" color="primary" fullWidth startIcon={<Face />}>Validate Face</Button>
+
+      
+      {loading && (
+              <HourglassEmpty style={{fontSize: '64px', color: 'black' }} />
+            )}
+      {walletConnected ? <Typography className="relative flex" variant="h5"> Linked wallet: {account}</ Typography> : <></>}
+      <Button variant="outlined" color={verificationComplete ? "success" : "primary"} fullWidth startIcon={<Face />} onClick={handleLogin2} style={{ marginTop: '10px'}}>Validate Face</Button>
       <Button variant="outlined" color="secondary" fullWidth startIcon={<Fingerprint />}>Validate Fingerprint</Button>
       <Button variant="outlined" color={walletConnected ? "success" : "secondary"} fullWidth startIcon={<AccountBalanceWallet />} onClick={handleLogin}>Connect Wallet</Button>
-
-      <Button variant="outlined" color="success" fullWidth style={{ marginTop: '10px' }} startIcon={<Check />} onClick={onClose}>Submit</Button>
+      <Button variant="outlined" color="success" fullWidth style={{ marginTop: '10px' }} startIcon={<Check />} onClick={handleSubmit} disabled={!verificationComplete || submitDisabled}>Submit</Button>
     </div>
   );
 };
